6beb4cee41f347981b44b66c89a3cf95d1e905b3
vagrant <vagrant@homestead>
2018-04-10 03:15:14 +0000

    basic user profile editing

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', $user->name . ' 的个人中心')
 
 @section('content')
-{{ $user->name }}
+
 <div class="row">
 
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
@@ -14,12 +14,14 @@
                         <img class="thumbnail img-responsive" src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" width="300px" height="300px">
                     </div>
                     <div class="media-body">
+                        <div class="media-body">
                         <hr>
                         <h4><strong>个人简介</strong></h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                        <p>{{ $user->introduction }}</p>
                         <hr>
                         <h4><strong>注册于</strong></h4>
-                        <p>January 01 1901</p>
+                        <p>{{ $user->created_at->diffForHumans() }}</p>
+                    </div>
                     </div>
                 </div>
             </div>
